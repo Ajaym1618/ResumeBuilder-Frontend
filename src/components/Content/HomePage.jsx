c249e850597f3fd0d1e92a9e8c90f8bf6502b754
Ajay M <114150731+Ajaym1618@users.noreply.github.com>
2024-07-02 17:21:44 +0530

    Update HomePage.jsx

--- src/components/Content/HomePage.jsx
+++ src/components/Content/HomePage.jsx
@@ -13,7 +13,7 @@ const HomePage = () => {
     <>
       <Header />
       <div className="w-screen h-[85vh] bg-[#100f0f] flex text-white max-[375px]:h-[85vh] max-[426px]:flex max-[426px]:flex-col-reverse">
-        <div className="w-[50%] flex items-center justify-center max-[768px]:w-full">
+        <div className="w-[50%] flex items-center justify-center max-[768px]:w-full max-[376px]:items-start">
           <img
             src={HI}
             alt="Home-Image"
