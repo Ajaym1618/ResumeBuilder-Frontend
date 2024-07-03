fe817a02025c2834e85422e4d22ad1d4a2ab3b9d
Ajay M <114150731+Ajaym1618@users.noreply.github.com>
2024-07-03 11:48:31 +0530

    Update signUp.jsx

--- src/components/Content/LoginSignUp/signUp.jsx
+++ src/components/Content/LoginSignUp/signUp.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { CheckCircleOutlined } from "@ant-design/icons";
+import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 
 const SignUp = () => {
   const [userInfo, setUserInfo] = useState([]);
@@ -128,7 +128,7 @@ const SignUp = () => {
       {signUpError && (
         <div className="fixed top-4 right-80 m-4 px-4 py-2 bg-white text-black rounded-md shadow-md max-[768px]:right-4 max-[768px]:top-0 max-[768px]:px-2 max-[768px]:py-2 max-[768px]:text-[12px]">
           <span className="flex gap-3 max-[768px]:gap-1">
-            <CheckCircleOutlined className="text-green-600" />
+            <CloseCircleOutlined className="text-green-600" />
             {signUpError}
           </span>
         </div>
