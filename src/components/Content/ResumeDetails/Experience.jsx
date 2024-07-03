c244364d78f26c4cefebc26bdc9a99d265a5b173
Ajay M <114150731+Ajaym1618@users.noreply.github.com>
2024-07-03 11:53:25 +0530

    Update Experience.jsx

--- src/components/Content/ResumeDetails/Experience.jsx
+++ src/components/Content/ResumeDetails/Experience.jsx
@@ -75,7 +75,7 @@ const Experience = () => {
   const handleSave = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.get(`https://swiftresume-backend.onrender.com/SignUpData/${user._id}`);
+      const response = await axios.get(`https://swiftresume-backend.onrender.com/signup/${user._id}`);
       const userData = response.data;
 
       const updatedUserData = {
