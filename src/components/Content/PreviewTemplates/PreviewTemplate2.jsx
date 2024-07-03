d774c260c5d6d3fb5cc2043c25deb7ee382cbac0
Ajay M <114150731+Ajaym1618@users.noreply.github.com>
2024-07-03 12:21:29 +0530

    Update PreviewTemplate2.jsx

--- src/components/Content/PreviewTemplates/PreviewTemplate2.jsx
+++ src/components/Content/PreviewTemplates/PreviewTemplate2.jsx
@@ -18,7 +18,7 @@ const PreviewTemplate2 = () => {
   const getData = async () => {
     try {
       const res = await axios.get(
-        `https://swiftresume-backend.onrender.com/signup/${user._id}`
+        `https://swiftresume-backend.onrender.com/SignUpData/${user._id}`
       );
       setUserInfo(res.data);
       console.log(res.data);
